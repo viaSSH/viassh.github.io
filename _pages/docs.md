ad4405f5bcaeff59eddd60c69efc9b3098554698
viassh <hanseung0609@gmail.com>
2021-01-06 14:08:43 +0900

    directory test

---
+++ _pages/docs.md
@@ -0,0 +1,7 @@
+---
+permalink: /docs/configuration
+title: "test"
+excerpt: "what is it"
+---
+
+hi test!
--- gitpush.sh
+++ gitpush.sh
@@ -2,7 +2,9 @@
 
 echo "Git Push process"
 echo "###=> " "$1"
-
+echo "\n"
+git config --global user.email "hanseung0609@gmail.com"
+git config --global user.name "viassh"
 git add .
 git commit -m "$1"
 git push -u origin main
